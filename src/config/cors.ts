8d8977503c8328bc7c2f4786ca33f06c5540f6b3
RadheyPrajapati <radheym2006@gmail.com>
2026-07-03 10:42:29 +0530

    Add support for Render domain in CORS

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -17,8 +17,9 @@ export const corsOptions: CorsOptions = {
 
     const isLocalhost = origin.includes("localhost") || origin.includes("127.0.0.1");
     const isPrivateIP = origin.match(/^https?:\/\/(192\.168\.|10\.|172\.(1[6-9]|2[0-9]|3[0-1])\.)/);
+    const isRenderDomain = origin.endsWith(".onrender.com");
 
-    if (isLocalhost || isPrivateIP || allowedOrigins.includes(origin)) {
+    if (isLocalhost || isPrivateIP || isRenderDomain || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
